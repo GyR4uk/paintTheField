ee68ea3529fa161eb60deecf827fffa90489b341
Дмитрий Сергеевич Попов <gyry96@gmail.com>
2019-12-10 16:39:11 +0300

    /

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-    var boxGames = document.querySelector('.boxGames');
+    let fieldGames = document.querySelector('.field'),
+        fieldSelect = document.querySelector('.select');
     const color = ["#d1ce00bb", //жёлтый
         "#d13100bb", //красный
         "#0050d1bb", //синий
@@ -8,24 +9,29 @@ window.onload = function() {
         "#00cad1bb" //берюзовый
     ];
 
-
     //задача рандомного цвета
     function randomColor() {
         let random = Math.floor(Math.random() * 6);
         return color[random];
     }
 
+
+
     function init() {
+        for (let i = 0; i < 6; i++) {
+            bigQuadrate = document.createElement('div');
+            bigQuadrate.className = 'bigQuadrate';
+            bigQuadrate.style.backgroundColor = color[i];
+            fieldSelect.appendChild(bigQuadrate);
+        }
+
         for (let i = 0; i < 256; i++) {
             quadrate = document.createElement('div');
             quadrate.className = 'quadrate'
             quadrate.style.backgroundColor = randomColor();
-            boxGames.appendChild(quadrate);
+            fieldGames.appendChild(quadrate);
         }
     }
-
-
-
 
     init();
 }
