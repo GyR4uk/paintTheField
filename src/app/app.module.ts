312a2dc4604fb85849a827e91ee17582ad1119d2
zapeace <zamirmikailov@gmail.com>
2019-12-23 18:40:32 +0300

    Stars component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from "./app.component";
 import { GameFiled } from "./game-field/game-filed.component";
@@ -34,7 +35,7 @@ import { LeaderTableComponent } from "./controls/control-leader-table/control-le
     GameDifficultyComoponent,
     LeaderTableComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule,BrowserAnimationsModule, FormsModule, HttpClientModule],
   providers: [GameService],
   bootstrap: [AppComponent]
 })
--- src/app/game-field/stars/stars.component.ts
+++ src/app/game-field/stars/stars.component.ts
@@ -1,10 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, Input,  OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: "app-stars",
   templateUrl: "./stars.component.html",
-  styleUrls: ["./stars.component.css"]
+  styleUrls: ["./stars.component.css"],
+  animations: [
+    trigger('endAnimation', [
+      state('end', style({
+        transform: 'rotate(360deg)'
+      })),
+      transition('* => end', animate('750ms'))])
+  ]
 })
-export class StarsComponent {
-  constructor() {}
+export class StarsComponent implements OnInit {
+  @Input() count: number;
+  maxSeconds = 30;
+  seconds = 30;
+  stars: Array<any>;
+  currentStar: number;
+  ngOnInit(): void {
+    this.stars = Array(this.count).fill(0).map((x,i)=>({
+      width: 100
+    }));
+    setInterval(() => {
+      this.seconds--;
+      
+      let partSize = this.maxSeconds / this.count;
+      const currentStarIndex = Math.ceil(this.seconds / partSize) - 1;
+      this.stars[currentStarIndex].width = this.seconds % partSize ? ((this.seconds % partSize) / partSize) * 100 : 100;
+      
+    }, 1000)
+  }
 }
