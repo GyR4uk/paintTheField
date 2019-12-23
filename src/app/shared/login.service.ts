15efaba9fe71b1d6cb204fe528a49419a77ef09c
ZCode <zelnskiroman@gmail.com>
2019-12-23 20:13:45 +0300

    HTTP pre-release v 1.0

--- src/app/shared/login.service.ts
+++ src/app/shared/login.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 export class LoginService {
   constructor(private ChatService: ChatService, private http: HttpClient) {}
-  isLogged: boolean = false;
+  isLogged: boolean = true;
 
   public _login(name: string): void {
     let headers = new HttpHeaders({
@@ -15,6 +15,7 @@ export class LoginService {
       })
       .subscribe(response => {
         this.isLogged = true;
+        this.ChatService.inrevalMessage();
       });
   }
 
